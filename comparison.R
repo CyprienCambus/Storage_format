2c8c37a28cefa111af049cbf4f6f0bfe3fbbacd1
CyprienCambus <cyprien.cambus@gmail.com>
2022-04-30 17:33:19 +0200

    Initial commit

---
+++ comparison.R
@@ -0,0 +1,46 @@
+library(microbenchmark)
+library(arrow)
+library(ggplot2)
+library(RSQLite)
+
+
+# Read the original data as csv 
+df_csv <- read.csv("data_uci.csv")
+
+# Save data on feather file, RData, rds
+
+# write_feather(df_csv,"data_uci.feather")
+# save(df_csv, file = "data_uci.RData")
+# saveRDS(df_csv, file = "data_uci.Rds")
+#write_parquet(df_csv,"data_uci.parquet")
+db <- dbConnect(SQLite(), "data_uci.sqlite")
+# dbWriteTable(db, "database", df_csv)
+
+
+
+# Comparing reading time for each method
+
+
+mbm <- microbenchmark("read_csv" = { 
+                        read.csv("data_uci.csv") 
+                      },
+                      "RData" = {
+                        load("data_uci.RData")
+                      },
+                      "RDS" = {
+                        readRDS("data_uci.Rds")
+                      },
+                      "sqlite" = {
+                        dbReadTable(db, "database")
+                      },
+                      "feather" = {
+                        read_feather("data_uci.feather")
+                      },
+                      "parquet" = {
+                        read_parquet("data_uci.parquet")
+                      },
+                      times = 150)
+mbm
+
+
+autoplot(mbm) + ggtitle("Storage format - Comparison of loading times")
